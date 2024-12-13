8975434cfa4c83f46db023b1aff24a0da195e9a5
olivererict <ericoliver52@gmail.com>
2024-12-13 15:46:56 -0600

    Refactoring and adding new functions

--- testing.py
+++ testing.py
@@ -16,25 +16,34 @@ CONNECTION_STRING = os.getenv('CONNECTION_STRING')
 REVIEWS_FOLDER = os.getenv('REVIEWS_FOLDER')
 
 for path, subdirs, files in os.walk(REVIEWS_FOLDER):
-	for name in files:
+
+	print(f'Dir path: {path}')
+	print(f'dir names: {subdirs}')
+	print(f'files: {files}')
+
+	#if any('album_review.md' in x for x in files):
+		#print(f'{path}: contains markdown')
+	
+	#for name in files:
 		
-		fullName = os.path.join(path,name)
-		if name[-16:].lower() == 'album_review2.md':
-			os.remove(fullName)
-		if name[-15:].lower() == 'album_review.md':
-			reviewFile = open(file=fullName,mode='r')
-			try:
-				lines = reviewFile.readlines()
-			except:
-				continue
-			part = lines[0][-3:-1]
-			reviewFile.close()
-			if part == "IP":
-				continue
-			os.remove(fullName)
+		#fullName = os.path.join(path,name)
+		#if name[-15:].lower() == 'album_review.md':
+			#print(f'Removing {fullName}')
+			#os.remove(fullName)
+		#if name[-15:].lower() == 'album_review.md':
+			#reviewFile = open(file=fullName,mode='r')
+			#try:
+				#lines = reviewFile.readlines()
+			#except:
+				#continue
+			#part = lines[0][-3:-1]
+			#reviewFile.close()
+			#if part == "IP":
+				#continue
+			#os.remove(fullName)
 			#review: Review.Review = ReviewParserService.ParseReviewMd(fullName)
 
-service = SQLService.SQLService(CONNECTION_STRING)
+#service = SQLService.SQLService(CONNECTION_STRING)
 
 # reviews = [review for review in reviews if review.artist]
 
@@ -47,25 +56,25 @@ service = SQLService.SQLService(CONNECTION_STRING)
 # 	for genre in review.genre:
 # 		success = service.InsertGenre(reviewId,genre)
 
-reviews = service.GetAllReviews()
+#reviews = service.GetAllReviews()
 
-for review in reviews:
-	parentPath = os.path.dirname(review.albumArt)
-	formattedTitle = review \
-		.title \
-		.replace(' ','_') \
-		.replace('*','') \
-		.replace(':','_') \
-		.replace('?','') \
-		.replace('/','_') \
-		.replace('...','')
-	newReviewPath = os.path.join(parentPath,f'{formattedTitle}_Album_Review.md')
-	reviewStr = ReviewParserService.ParseReviewObj(review)
+#for review in reviews:
+	#parentPath = os.path.dirname(review.albumArt)
+	#formattedTitle = review \
+	#	.title \
+	#	.replace(' ','_') \
+	#	.replace('*','') \
+	#	.replace(':','_') \
+	#	.replace('?','') \
+	#	.replace('/','_') \
+	#	.replace('...','')
+	#newReviewPath = os.path.join(parentPath,f'{formattedTitle}_Album_Review.md')
+	#reviewStr = ReviewParserService.ParseReviewObj(review)
 
-	try:
-		file = open(newReviewPath,'w')
-		file.writelines(reviewStr)
-	except:
-		print('Fucking Halley Labs and Special Chars:' + newReviewPath)
+	#try:
+	#	file = open(newReviewPath,'w')
+	#	file.writelines(reviewStr)
+	#except:
+	#	print('Fucking Halley Labs and Special Chars:' + newReviewPath)
 
 #service.TestQuery()
